b8ad910ce8be87edca8a1c0416234708f11a9869
Anton Kukhovarenko <a.kukhovarenko@innopolis.ru>
2025-01-18 19:00:50 +0300

    load from file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::{File, OpenOptions}, io::{self, BufRead, BufReader, BufWriter, ErrorKind, Seek, SeekFrom, Write} , path::Path};
+use std::{collections::HashMap, fs::{File, OpenOptions}, io::{self, BufRead, BufReader, BufWriter, Read, Seek, SeekFrom, Write} , path::Path};
 use byteorder::LittleEndian;
 use byteorder::{ReadBytesExt, WriteBytesExt};
 use serde_derive::{Serialize, Deserialize};
@@ -20,12 +20,14 @@ pub struct ActionKV {
 
 impl ActionKV {
     pub fn process_record<R: BufRead>(f: &mut R) -> io::Result<KeyValuePair> {
-        let mut buffer = [0;2];
-        let result = f.read(&mut buffer)?;
-        if result == 0 {
-            return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
-        }
-        Ok(KeyValuePair {key: vec![buffer[0]], value: vec![buffer[1]]})
+        let key_len = f.read_u32::<LittleEndian>()?;
+        let value_len = f.read_u32::<LittleEndian>()?;
+        let mut key = ByteString::with_capacity(key_len as usize);
+        f.take(key_len as u64).read_to_end(&mut key)?;
+        let mut value = ByteString::with_capacity(value_len as usize);
+        f.take(value_len as u64).read_to_end(&mut value)?;
+        
+        Ok(KeyValuePair {key, value})
     }
 
     pub fn open(path: &Path) -> io::Result<Self> {
@@ -81,6 +83,7 @@ impl ActionKV {
         f.write_u32::<LittleEndian>(key_len as u32)?;
         f.write_u32::<LittleEndian>(value_len as u32)?;
         f.write_all(&tmp)?;
+        f.flush()?;
 
         self.index.insert(key.to_vec(), position);
 
@@ -103,7 +106,8 @@ mod tests {
     }
 
     fn get_test_action_kv() -> ActionKV {
-        ActionKV {f: init_file(Vec::from(b"abcd").as_mut()), index: HashMap::new()}
+        let mut data: ByteString = vec![1, 0, 0, 0, 1, 0, 0, 0, 0xAA, 0xBB, 1, 0, 0, 0, 2, 0, 0, 0, 0xCC, 0xDD, 0xEE];
+        ActionKV {f: init_file(&mut data), index: HashMap::new()}
     }
 
     #[test]
@@ -118,11 +122,15 @@ mod tests {
     fn actionkv_insert(){
         let mut tempfile = NamedTempFile::new().unwrap();
         let mut  store = ActionKV::open(tempfile.path()).unwrap();
-
         let path = tempfile.path();
-        let result = store.insert(&[0xAA, 0xAB, 0xAC], &[130, 140, 150]);
-        let result = store.insert(&[150, 160, 170], &[180, 190, 200]);
-        tempfile.flush();
+        store.insert(&[0xAA], &[130, 140, 150]).unwrap();
+        store.insert(&[0xBB], &[180, 190, 200]).unwrap();
+
+        assert_eq!(store.index.get(&[0xAA].to_vec()).unwrap(), &0u64);
+        assert_eq!(store.index.get(&[0xBB].to_vec()).unwrap(), &12u64);
+
+        tempfile.flush().unwrap();
+        assert_eq!(tempfile.as_file().metadata().unwrap().len(), 24);
     }
 
     #[test]
@@ -135,15 +143,15 @@ mod tests {
 
     #[test]
     fn actionkv_process_record() {
-        let f = init_file(Vec::from(b"abcd").as_mut());
+        let f = init_file(vec![1, 0, 0, 0, 1, 0, 0, 0, 0xAA, 0xBB, 1, 0, 0, 0, 2, 0, 0, 0, 0xCC, 0xDD, 0xEE].as_mut());
         let mut buffer = BufReader::new(f);
         let kv_pair = ActionKV::process_record(&mut buffer);
         assert!(kv_pair.is_ok());
-        assert_eq!(kv_pair.unwrap(), KeyValuePair {key: vec![b'a'], value: vec![b'b']});
+        assert_eq!(kv_pair.unwrap(), KeyValuePair {key: vec![0xAA], value: vec![0xBB]});
         
         let kv_pair  = ActionKV::process_record(&mut buffer);
         assert!(kv_pair.is_ok());
-        assert_eq!(kv_pair.unwrap(), KeyValuePair {key: vec![b'c'], value: vec![b'd']});
+        assert_eq!(kv_pair.unwrap(), KeyValuePair {key: vec![0xCC], value: vec![0xDD, 0xEE]});
 
         let kv_pair = ActionKV::process_record(&mut buffer);
         assert!(kv_pair.is_err_and(|x| x.kind() == io::ErrorKind::UnexpectedEof));
@@ -152,10 +160,10 @@ mod tests {
     #[test]
     fn actionkv_load() {
         let mut store = get_test_action_kv();
-        let key: &ByteStr = b"c";
+        let key: &ByteStr = &[0xCCu8];
         let result = store.load();
         assert!(result.is_ok());
         let data = store.index.get(key).unwrap();
-        assert_eq!(*data, 2);
+        assert_eq!(*data, 10);
     }
 }
